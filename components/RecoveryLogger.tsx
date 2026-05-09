cdfd27110cdb150a6faee1c0f8086400106e1223
ericbrenneman <eric.brenneman@gmail.com>
2026-05-09 21:05:57 +0000

    fix: padding bottom recoverlogger.tsx

--- components/RecoveryLogger.tsx
+++ components/RecoveryLogger.tsx
@@ -116,7 +116,7 @@
           };
 
           return (
-            <div className="flex flex-col flex-1 pb-10">
+            <div className="flex flex-col flex-1 pb-28">
               <div className="flex items-center justify-between px-4 pt-10 pb-4">
                 <div>
                   <p className="text-muted text-xs mb-0.5">
